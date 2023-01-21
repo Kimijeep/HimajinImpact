edde3536d8d505fdd7a34cf6931673b4124c2255
kimijeep <kimijeepa2@gmail.com>
2023-01-22 03:27:05 +0900

    updated

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const port = 3000;
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+	response.send('<h1>Hello Express.js!</h1>');
+}).listen(port, () => {
+	console.log(`The server has started and is listening on port number: ${port}`);
+});
+
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -37,6 +47,7 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
 
 // Log in to Discord with your client's token
 client.login(token);
